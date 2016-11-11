3bc684e4652f2a30bc238ef876cc709ab9a92f25
blueberryboll <inkblot_cartoon@yahoo.se>
2016-11-11 17:24:17 +0100

    Set up folder structure and basic play area with player movement

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float movementX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float movementY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(movementX, movementY, 0);
+    }
+}
