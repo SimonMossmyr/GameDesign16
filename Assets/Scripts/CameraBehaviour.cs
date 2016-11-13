30ba3af00ca9fd1d08acd554a3e4978f59052b3d
Mats <mats.stichel@gmail.com>
2016-11-13 23:25:57 +0100

    Added first version of camera movement

---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraBehaviour : MonoBehaviour {
+
+	private GameObject player;
+	private Camera c;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("PlayerCharacter");
+		c = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 vPos = c.WorldToViewportPoint (player.transform.position);
+
+		Vector2 newPos = new Vector2();
+
+		if (vPos.x < 0.2f) {
+			newPos.x -= 0.2f - vPos.x;
+		} else if (vPos.x > 0.8f) {
+			newPos.x += vPos.x - 0.8f;
+		}
+
+		if (vPos.y < 0.2f) {
+			newPos.y -= 0.2f - vPos.y;
+		} else if (vPos.y > 0.8f) {
+			newPos.y += vPos.y - 0.8f;
+		}
+
+		gameObject.transform.Translate (newPos);
+	}
+}
