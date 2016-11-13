a47e6ebd47b62c677a402f17a33e7d5082dc478b
Mats <mats.stichel@gmail.com>
2016-11-13 22:25:05 +0100

    Improved player movement and collisions

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,18 +3,47 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-    public float speed = 2f;
+    public float speed = 1f;
+	private Rigidbody2D rb2d;
+	private bool up = false, down = false, left = false, right = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		rb2d = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float movementX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float movementY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+		updateMovement ();
 
-        transform.Translate(movementX, movementY, 0);
+		float movementX = (right && !left? 1 : (left && !right? -1 : 0)) * Time.deltaTime * speed;
+		float movementY = (up && !down? 1 : (down && !up? -1 : 0)) * Time.deltaTime * speed;
+
+		Vector2 movement = new Vector2 (movementX, movementY);
+
+		// rb2d.AddForce (movement * speed);
+		rb2d.MovePosition (rb2d.position + (movement * speed));
     }
+
+	void updateMovement () {
+		if (Input.GetKeyDown (KeyCode.W) && !up)
+			up = true;
+		else if (Input.GetKeyUp (KeyCode.W) && up)
+			up = false;
+
+		if (Input.GetKeyDown (KeyCode.S) && !down)
+			down = true;
+		else if (Input.GetKeyUp (KeyCode.S) && down)
+			down = false;
+
+		if (Input.GetKeyDown (KeyCode.A) && !left)
+			left = true;
+		else if (Input.GetKeyUp (KeyCode.A) && left)
+			left = false;
+
+		if (Input.GetKeyDown (KeyCode.D) && !right)
+			right = true;
+		else if (Input.GetKeyUp (KeyCode.D) && right)
+			right = false;
+	}
 }
