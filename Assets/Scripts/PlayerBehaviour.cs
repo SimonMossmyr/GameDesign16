03e1a43999de9dee5f20966d213e97b480e464ff
Simon Mossmyr <mossmyr@live.com>
2016-11-14 17:32:35 +0100

    reverted back to blueberry's movement system

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-    public float speed = 1f;
+    public float speed;
 	private Rigidbody2D rb2d;
 
 	// Hold whether direction key is pressed
@@ -16,42 +16,16 @@ public class PlayerBehaviour : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		updateMovement ();
-
 		/*
 		 * Calculate directional movement.
-		 * Movement per frame is is (1|-1) * delta * speed.
 		 */
-		float movementX = (right && !left? 1 : (left && !right? -1 : 0)) * Time.deltaTime * speed;
-		float movementY = (up && !down? 1 : (down && !up? -1 : 0)) * Time.deltaTime * speed;
+		float movementX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		float movementY = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
+		/*
+		 * Execute movement
+		 */
 		Vector2 movement = new Vector2 (movementX, movementY);
-
-		rb2d.MovePosition (rb2d.position + (movement * speed));
+		rb2d.MovePosition (rb2d.position + movement);
     }
-
-	/*
-	 * Update bools holding key-down information
-	 */
-	void updateMovement () {
-		if (Input.GetKeyDown (KeyCode.W) && !up)
-			up = true;
-		else if (Input.GetKeyUp (KeyCode.W) && up)
-			up = false;
-
-		if (Input.GetKeyDown (KeyCode.S) && !down)
-			down = true;
-		else if (Input.GetKeyUp (KeyCode.S) && down)
-			down = false;
-
-		if (Input.GetKeyDown (KeyCode.A) && !left)
-			left = true;
-		else if (Input.GetKeyUp (KeyCode.A) && left)
-			left = false;
-
-		if (Input.GetKeyDown (KeyCode.D) && !right)
-			right = true;
-		else if (Input.GetKeyUp (KeyCode.D) && right)
-			right = false;
-	}
 }
