6b76d3b77767324f6c63ebd4d017296e5b24e736
blueberryboll <inkblot_cartoon@yahoo.se>
2016-11-14 19:25:49 +0100

    Implemented Simons changes

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour {
+
+	[Range(0,1)]
+	public int PlayerNumber;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
