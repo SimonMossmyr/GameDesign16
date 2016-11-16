bacfe719943de6d835738470eb482af7f57ea2fb
Simon Mossmyr <mossmyr@live.com>
2016-11-16 18:45:24 +0100

    added heal bomb and slow bomb

---
+++ Assets/Scripts/Explosion Scripts/AddSlowScriptToPlayers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddSlowScriptToPlayers : MonoBehaviour {
+
+	public float Radius;
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+
+		foreach (GameObject player in players) {
+			if (Vector2.Distance (transform.position, player.transform.position) <= Radius) {
+				player.AddComponent<SlowScript>();
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
