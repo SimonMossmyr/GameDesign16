1608cfd497566d89337872400a58b7f0b3cd7d29
halitanildonmez <halitanil.donmez@gmail.com>
2016-11-23 17:17:22 +0100

    bomb slot fixes, crafting error fix, removed material list

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -7,9 +7,12 @@ public class InventoryManager : MonoBehaviour {
     //inventory of the player
     private List<Material> itemsInInventory = new List<Material>();
 
+
+    int material1Count, material2Count, material3Count;
+
     // Use this for initialization
     void Start () {
-
+        material1Count = material2Count = material3Count = 0;
     }
 	
 	// Update is called once per frame
@@ -27,13 +30,62 @@ public class InventoryManager : MonoBehaviour {
         itemsInInventory = val;
     }
 
+    public int getMaterial1Count()
+    {
+        return material1Count;
+    }
+    public void setMaterial1Count(int val)
+    {
+        material1Count = val;
+    }
+    public void incrementMaterial1()
+    {
+        material1Count++;
+    }
+
+
+    public int getMaterial2Count()
+    {
+        return material2Count;
+    }
+    public void setMaterial2Count(int val)
+    {
+        material2Count = val;
+    }
+    public void incrementMaterial2()
+    {
+        material2Count++;
+    }
+
+    public int getMaterial3Count()
+    {
+        return material3Count;
+    }
+    public void setMaterial3Count(int val)
+    {
+        material3Count = val;
+    }
+    public void incrementMaterial3()
+    {
+        material3Count++;
+    }
+
     //collision with the materials in the scene
     void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log(other.gameObject.name);
+        //Debug.Log(other.gameObject.name);
 
         if ( other.gameObject.tag == "Material")
         {
+            Material collidedMaterial = other.gameObject.GetComponent<Material>();
+
+            if (collidedMaterial.getMaterialType() == Material.MaterialType.Material1)
+                material1Count++;
+            else if (collidedMaterial.getMaterialType() == Material.MaterialType.Material2)
+                material2Count++;
+            else
+                material3Count++;
+
             itemsInInventory.Add(other.gameObject.GetComponent<Material>());
 
             Destroy(other.gameObject);
