720b84551db3331a019d08ea71d785510692b946
halitanildonmez <halitanil.donmez@gmail.com>
2016-11-28 19:20:44 +0100

    updated bomb crafting to standart bomb, accumulates material attributes

--- Assets/Scripts/Bomb Scripts/BombBehaviour.cs
+++ Assets/Scripts/Bomb Scripts/BombBehaviour.cs
@@ -3,13 +3,33 @@ using System.Collections;
 
 public class BombBehaviour : MonoBehaviour {
 
+    public int damage, effect, range;
+
 	// Use this for initialization
 	void Start () {
-	
+        damage = effect = range = 0;
 	}
 	
+    public void setDamage(int val)
+    {
+        Debug.Log("called");
+        damage = val;
+    }
+
+    public void setEffect(int val)
+    {
+        Debug.Log("called2");
+        effect = val;
+    }
+
+    public void setRange(int val)
+    {
+        Debug.Log("called3");
+        range = val;
+    }
+
 	// Update is called once per frame
 	void Update () {
-	
+        Debug.Log(damage + " --- " + effect + " ---  " + range);
 	}
 }
