e168099c1f4b63d3d839ac3c26010321f6857e55
Maydell <mats.stichel@gmail.com>
2016-11-28 13:28:29 +0100

    First version of multiplayer

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : NetworkBehaviour {
 
     public float speed;
 	private Rigidbody2D rb2d;
-	private int playerNumber;
 
     private bool isFacingEast;
     private bool isFacingNorth;
@@ -13,43 +13,55 @@ public class PlayerMovement : MonoBehaviour {
     // Use this for initialization
     void Start () {
 		rb2d = GetComponent<Rigidbody2D> ();
-		playerNumber = gameObject.GetComponent<PlayerStats> ().PlayerNumber;
 	}
 	
 	// FixedUpdate is called once per physics frame
 	void FixedUpdate () {
+		if (!isLocalPlayer)
+		{
+			return;
+		}
+
 		/*
 		 * Calculate directional movement.
 		 */
-		float movementX = Input.GetAxisRaw("Horizontal-P"+playerNumber) * Time.deltaTime * speed;
-		float movementY = Input.GetAxisRaw("Vertical-P"+playerNumber) * Time.deltaTime * speed;
 
-        if(movementX > 0.0001)
+		float movementX = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+		float movementY = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+		float rotation = 0;
+
+		if(movementX > 0.0001)
         {
             isFacingEast = true;
             isFacingNorth = false;
+			rotation = 0;
         }
         else if (movementX < -0.0001)
         {
             isFacingEast = false;
             isFacingNorth = false;
+			rotation = 180;
         }
 
         if (movementY > 0.0001)
         {
             isFacingNorth = true;
-            isFacingEast = false;
+			isFacingEast = false;
+			rotation = 270;
         }
         else if (movementY < -0.0001)
         {
             isFacingNorth = false;
-            isFacingEast = false;
+			isFacingEast = false;
+			rotation = 90;
         }
 
         /*
 		 * Execute movement
 		 */
         Vector2 movement = new Vector2 (movementX, movementY);
+		Debug.Log (rotation);
+		rb2d.MoveRotation (rotation);
 		rb2d.MovePosition (rb2d.position + movement);
     }
 
