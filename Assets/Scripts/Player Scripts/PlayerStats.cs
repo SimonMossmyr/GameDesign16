836bd48c02d0321ad677f82b8d9a4ff7e01873d4
Mats Stichel <mstichel@kth.se>
2016-12-02 16:14:38 +0100

    Adding playernumber on connect

--- Assets/Scripts/Player Scripts/PlayerStats.cs
+++ Assets/Scripts/Player Scripts/PlayerStats.cs
@@ -14,12 +14,12 @@ public class PlayerStats : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
 	public void AddHealth(float delta) {
